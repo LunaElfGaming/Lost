ffaf58eae768e89ce0ac32f68d8a9a196caabe0b
煌瞳小妖 <892611452@qq.com>
2019-08-03 23:05:47 +0800

    1

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -36,7 +36,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D other) {
+    private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.tag == "death")
         {
             GameManager.GM.ResetLevel();
